1b39a7f7b5f9b3e10c5b4ec6a852e174b07e7381
Clara Olson <cjolson@andrew.cmu.edu>
2018-11-15 13:03:54 -0500

    scroll magic practice

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () { // wait for document ready
+		// init
+		var controller = new ScrollMagic.Controller();
 
+		// define movement of panels
+		var wipeAnimation = new TimelineMax()
+			.fromTo("section.panel.mensturation-start",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
 
-}),
+		// create scene to pin and link animation
+		var scene = new ScrollMagic.Scene({
+				triggerElement: "#content-container",
+				triggerHook: "onLeave",
+				duration: "300%"
+			})
+			.setPin("mensturation-start")
+			.setTween(wipeAnimation)
+			.addIndicators() // add indicators (requires plugin)
+			.addTo(controller);
+	});
