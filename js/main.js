65ce1d0c9699c716f10e0649e8bca3e588073923
Clara Olson <cjolson@andrew.cmu.edu>
2018-11-16 14:13:15 -0500

    Pin nav menu with horizontal scroll

--- js/main.js
+++ js/main.js
@@ -2,17 +2,24 @@ $(document).ready(function () {
 
   // init controller
   var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      reverse: true
+    },
     vertical: false,
   });
 
-/*
+
+
   new ScrollMagic.Scene({
     triggerElement: '.timeline-nav',
-    triggerHook: 'onEnter',
+    triggerHook: '0',
+    pushFollowers: true,
+    duration: '1000%',
   })
   .setPin('.timeline-nav')
   .addIndicators()
   .addTo(controller);
-  */
+
+
 
 });
