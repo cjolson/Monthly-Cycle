1ceefb60c6ca72de516581af97fa2d3518f5d2af
Clara Olson <cjolson@andrew.cmu.edu>
2018-11-15 21:20:48 -0500

    Slide wipes working.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
-$(document).ready(function () { // wait for document ready
-		// init
-		var controller = new ScrollMagic.Controller();
+$(document).ready(function () {
 
-		// define movement of panels
-		var wipeAnimation = new TimelineMax()
-			.fromTo("section.panel.mensturation-start",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
+  // init controller
+  var controller = new ScrollMagic.Controller();
+/*
+  var timeline = new ScrollMagic.Scene({
+    triggerElement: '#timeline-nav',
+    triggerHook: 0.9,
+    reverse: true,
+  })
+    .setPin('#timeline-nav')
+    .addIndicators()
+    .addTo(controller);
+*/
+  var wipeAnimation = new TimelineMax()
+    .fromTo('section.panel.menstruation-start',
+            1, {x:  '100%'}, {x: '0%', ease: Linear.easeNone})
+    .fromTo('section.panel.follicular-phase',
+            1, {x:  '100%'}, {x: '0%', ease: Linear.easeNone})
+    .fromTo('section.panel.ovulation',
+            1, {x:  '100%'}, {x: '0%', ease: Linear.easeNone})
+    .fromTo('section.panel.luteal-phase',
+            1, {x:  '100%'}, {x: '0%', ease: Linear.easeNone})
+    .fromTo('section.panel.menstruation-end',
+            1, {x:  '100%'}, {x: '0%', ease: Linear.easeNone});
 
-		// create scene to pin and link animation
-		var scene = new ScrollMagic.Scene({
-				triggerElement: "#content-container",
-				triggerHook: "onLeave",
-				duration: "300%"
-			})
-			.setPin("mensturation-start")
-			.setTween(wipeAnimation)
-			.addIndicators() // add indicators (requires plugin)
-			.addTo(controller);
-	});
+  var scene = new ScrollMagic.Scene({
+    triggerElement: '#content-container',
+    triggerHook: 'onLeave',
+    duration: '300%',
+    reverse: true,
+  })
+    .setPin('#content-container')
+    .setTween(wipeAnimation)
+    .addIndicators()
+    .addTo(controller);
+
+});
