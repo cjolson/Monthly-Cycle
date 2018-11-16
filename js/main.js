d18c0acbad485f846eb282ddf4727ccad38a00ee
Clara Olson <cjolson@andrew.cmu.edu>
2018-11-16 18:07:34 -0500

    Added more Greensock stuff. Also the anchor menu kinda works, but not.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 $(document).ready(function () {
 
   // init controller
-  var controller = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      reverse: true
-    },
-    vertical: false,
+  var controller = new ScrollMagic.Controller();
+
+  // animate scroll instead of a jump
+  controller.scrollTo(function(target) {
+    console.log('scroooooll');
+    console.log('target = '+target); // BUT HERE IT IS 0 :cry:
+    console.log(typeof(target));
+    /*TweenMax.to(window, 0.5, { // .5 is scroll speed
+      scrollTo: {
+        y: id,        // scroll along y axis
+        autoKill: true,   // allow users to kill scroll action
+      },
+      ease: Cubic.easeInOut,
+    });*/
   });
 
-
-
-  new ScrollMagic.Scene({
-    triggerElement: '.timeline-nav',
-    triggerHook: '0',
-    pushFollowers: true,
-    duration: '1000%',
-  })
-  .setPin('.timeline-nav')
-  .addIndicators()
-  .addTo(controller);
-
-
+  // scroll action when you click the nav links
+  $(document).on('click', 'a[href^=#]', function(e) {
+    var id = $(this).attr('href'); // get the href of clicked link
+    if ($(id).length > 0) { // not empty links
+      e.preventDefault(); // prevent normal link action
+      console.log('click');
+      console.log('id = '+id); // IT PRINTS CORRECT HERE
+      console.log(typeof(id));
+      controller.scrollTo(id); // scroll on click
+      // update the URL
+      if (window.history && window.history.pushState) {
+        history.pushState("", document.title, id);
+      }
+    }
+  });
 
 });
